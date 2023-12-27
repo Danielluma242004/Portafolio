250462a0bddb6c61eb27883a317debf0b9d6694d
Daniel Lucio <danielluma242004@hotmail.com>
2023-12-27 03:45:48 -0600

    Home Fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import SplitType from "split-type";
 import gsap from "gsap";
 import scroll from "../media/scroll-down.svg";
