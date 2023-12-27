0645be65f497abcbd92d52d94c344f3c4db4d870
Daniel Lucio <danielluma242004@hotmail.com>
2023-12-26 18:02:21 -0600

    Contact and Footer Sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import fotoMia from "../media/foto_mia.jpg";
+import cv from "../media/CV-Daniel_Lucio_Mireles_en.pdf";
 
 export function About() {
   return (
@@ -9,18 +10,21 @@ export function About() {
           <h2 className="mx-4 text-cyan-700 font-bold text-xl">About me</h2>
           <div className="h-px bg-gray-400 flex-grow"></div>
         </div>
-        <div className="flex flex-col md:flex-row items-center bg-gray-100 rounded-3xl p-5">
-          <div className="w-full md:w-1/2 text-center md:text-left mx-auto my-5">
+        <div className="flex flex-col md:flex-row items-center bg-gray-100 rounded-3xl py-5 px-6 md:px-0">
+          <div className="w-full md:w-1/2 text-center md:text-left md:mx-11 my-5">
             <p className=" font-bold text-cyan-700 text-3xl pb-5">
               Daniel Lucio
             </p>
             <p>
-              Desarrollador web apasionado y creativo, especializado en la
-              creación de soluciones innovadoras. Con experiencia en
-              planificación estratégica, busco contribuir a proyectos
-              emocionantes con mi dedicación y conocimientos técnicos.
+              Passionate and creative web developer, specialized in creation of
+              innovative solutions. With experience in strategic planning, I
+              seek to contribute to projects exciting with my dedication and
+              technical knowledge.
             </p>
-            <button className="bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white font-bold mt-5 py-2 px-4 rounded inline-flex items-center">
+            <a
+              className="bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white font-bold mt-5 py-2 px-4 rounded inline-flex items-center"
+              href={cv}
+            >
               <svg
                 className="fill-current w-4 h-4 mr-2"
                 xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +33,7 @@ export function About() {
                 <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
               </svg>
               <span>Download CV</span>
-            </button>
+            </a>
           </div>
           <img
             src={fotoMia}
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,54 @@
 export function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
+    <div className="flex items-center justify-center h-screen">
+      <section className="max-w-5xl mx-4 flex flex-col w-full">
+        <div className="flex items-center mb-8">
+          <div className="h-px bg-gray-400 flex-grow"></div>
+          <h2 className="mx-4 text-cyan-700 font-bold text-xl">Contact</h2>
+          <div className="h-px bg-gray-400 flex-grow"></div>
+        </div>
+        <div className="bg-gray-100 rounded-3xl p-5 md:p-8 w-full">
+          <form action="POST" data-netlify="true" className="m-3 mb-0">
+            <p className="font-bold text-cyan-700 text-3xl pb-5">
+              Get in touch!
+            </p>
+            <div className="flex flex-col md:flex-row">
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                  className="w-full p-2 border rounded mb-4"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  className="w-full p-2 border rounded mb-4"
+                />
+              </div>
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Message"
+                rows={3}
+                className="w-full p-2 border rounded mb-4 md:ml-4"
+              ></textarea>
+            </div>
+            <div data-netlify-recaptcha="true"></div>
+            <div className="flex justify-end">
+              <input
+                type="submit"
+                value="Send Message"
+                className="bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white font-bold py-2 px-4 rounded"
+              />
+            </div>
+          </form>
+        </div>
+        <div></div>
+      </section>
     </div>
   );
 }
