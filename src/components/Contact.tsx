26cc4c8de050682f6544e6aa73cbde4d4bfaf62b
Daniel Lucio <danielluma242004@hotmail.com>
2024-01-02 13:56:34 -0600

    contact fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,25 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+
 export function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Lógica para enviar datos a Netlify o hacer otras acciones con los datos
+  };
   return (
     <div className="flex items-center justify-center pb-10" id="contact">
       <section className="max-w-5xl mx-4 flex flex-col w-full">
@@ -15,6 +36,7 @@ export function Contact() {
             name="contact"
             method="POST"
             data-netlify="true"
+            onSubmit={handleSubmit}
           >
             <input type="hidden" name="form-name" value="contact" />
             <input type="hidden" name="bot-field" />
@@ -26,6 +48,8 @@ export function Contact() {
                 <input
                   type="text"
                   name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
                   id="name"
                   placeholder="Name"
                   className="w-full p-2 rounded mb-4 dark:bg-gray-700"
@@ -33,6 +57,8 @@ export function Contact() {
                 <input
                   type="email"
                   name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
                   id="email"
                   placeholder="Email"
                   className="w-full p-2 rounded mb-4  dark:bg-gray-700"
@@ -40,6 +66,8 @@ export function Contact() {
               </p>
               <textarea
                 name="message"
+                value={formData.message}
+                onChange={handleInputChange}
                 id="message"
                 placeholder="Message"
                 rows={3}
